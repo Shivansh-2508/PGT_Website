8bfa3fd524e1bcfad5d4c7b17278a45a6e3239e5
shivansh <shivansh.kukreti.2004@gmail.com>
2024-01-30 16:27:09 +0530

    navbar_close

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,15 @@
-"use client";
-
-import MenuIcon from "@mui/icons-material/Menu";
-import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
-import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import InstagramIcon from "@mui/icons-material/Instagram";
-import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
-import LightModeRoundedIcon from "@mui/icons-material/LightModeRounded";
+import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 import { useTheme } from "next-themes";
 import { Link } from "react-scroll";
 import Image1 from "../../public/dark_logo.svg";
 import Image2 from "../../public/light_logo.svg";
-import Image from "next/image";
-
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
+import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import LightModeRoundedIcon from "@mui/icons-material/LightModeRounded";
 
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
@@ -22,42 +17,71 @@ const Navbar = () => {
   const { theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
   const navbar = useRef();
+  const overlay = useRef();
 
   useEffect(() => {
-    window.onscroll = () => {
-      setMounted(true);
-      if (window.pageYOffset >= 0) {
-        navbar.current.classList.add("shadow");
-      } else {
-        navbar.current.classList.remove("shadow");
-      }
-    };
+    setMounted(true);
   }, []);
+
+  useEffect(() => {
+    if (mounted) {
+      window.onscroll = () => {
+        if (window && window.pageYOffset >= 0) {
+          navbar.current.classList.add("shadow");
+        } else {
+          navbar.current.classList.remove("shadow");
+        }
+      };
+    }
+    return () => {
+      window.onscroll = null;
+    };
+  }, [mounted]);
+
+  useEffect(() => {
+    if (mounted) {
+      // Set dark theme as default
+      setTheme("dark");
+    }
+  }, [mounted, setTheme]);
+
+  useEffect(() => {
+    const closeMenu = () => {
+      setToggleMenu(false);
+    };
+
+    if (toggleMenu) {
+      overlay.current.addEventListener("click", closeMenu);
+    } else {
+      overlay.current.removeEventListener("click", closeMenu);
+    }
+
+    return () => {
+      overlay.current.removeEventListener("click", closeMenu);
+    };
+  }, [toggleMenu]);
 
   return (
     <div
       ref={navbar}
       className={`${
         theme === "dark" ? "bg-[#121212]" : "bg-white text-black"
-      } w-full z-50 fixed top-0 left-0 py-4 mb-10`}>
-      <div className='container px-5 md:px-16 flex items-center justify-between mx-auto'>
+      } w-full z-50 fixed top-0 left-0 pt-1 mb-10`}
+    >
+      <div className="container px-5 md:px-16 flex items-center justify-between mx-auto ">
         <Link to={"/"} smooth={true} offset={0} duration={1000}>
-          <h2 className='text-3xl'>
+          <h2 className="text-3xl">
             {theme === "dark" ? (
-              <Image
-                src={Image1}
-                alt='Dark Image'
-                className='w-full sm:w-1/2'
-              />
+              <Image src={Image1} alt="Dark Image" width={100} />
             ) : (
-              <Image src={Image2} alt='light' className='w-full sm:w-1/2' />
+              <Image src={Image2} alt="light" width={100} />
             )}
-
           </h2>
         </Link>
 
         <div>
           <ul
+            ref={overlay}
             className={`${toggleMenu === true ? "left-0" : "-left-full"} ${
               theme === "dark"
                 ? "bg-[#121212] text-white"
@@ -72,26 +96,21 @@ const Navbar = () => {
             >
               <CloseOutlinedIcon />
             </button>
-            {[
-              "home",
-              "features",
-              "pricing",
-              "Connect",
-              "Maps",
-              "testimonial",
-            ].map((link) => (
-              <li
-                key={link}
-                className={`${
-                  selectedItem === link ? "text-rose-600" : ""
-                } capitalize border-b py-4 md:border-none md:py-0 hover:text-rose-600 cursor-pointer`} // Add cursor-pointer class
-                onClick={() => setSelectedItem(link)}>
-
-                <Link to={`${link}`} smooth={true} offset={0} duration={1000}>
-                  {link}
-                </Link>
-              </li>
-            ))}
+            {["home", "features", "pricing", "Maps", "testimonial"].map(
+              (link) => (
+                <li
+                  key={link}
+                  className={`${
+                    selectedItem === link ? "text-rose-600" : ""
+                  } capitalize border-b py-4 md:border-none md:py-0 hover:text-rose-600 cursor-pointer`}
+                  onClick={() => setSelectedItem(link)}
+                >
+                  <Link to={`${link}`} smooth={true} offset={0} duration={1000}>
+                    {link}
+                  </Link>
+                </li>
+              )
+            )}
 
             <div className="md:hidden mx-auto absolute bottom-16 left-1/2 -translate-x-1/2 flex gap-3">
               <Link href="#" target="_blank">
@@ -100,18 +119,18 @@ const Navbar = () => {
               <Link target="_blank" href={"#"}>
                 <LinkedInIcon className="cursor-pointer hover:text-rose-600 text-xl" />
               </Link>
-
-              <Link target="_blank" href={"#"}>
-                <InstagramIcon className="cursor-pointer hover:text-rose-600 text-xl" />
-              </Link>
             </div>
           </ul>
         </div>
 
-
         <div className="flex items-center gap-2 sm:gap-4 md:gap-2 lg:gap-4">
-          <button className="capitalize text-sm sm:text-base border-2 hover:border-2 font-semibold sm:py-3 py-2 px-3 sm:px-6 text-rose-600 border-rose-600 hover:border-rose-600 hover:bg-rose-600 hover:text-white rounded-full">
-            <Link href={"#pricing"}>Get Started</Link>
+          <button
+            className="capitalize text-sm sm:text-base border-2 hover:border-2 font-semibold sm:py-3 py-2 px-3 sm:px-6 text-rose-600 border-rose-600 hover:border-rose-600 hover:bg-rose-600 hover:text-white rounded-full"
+            onClick={() => setSelectedItem(Connect)}
+          >
+            <Link to="Connect">
+              <a>Contact Us</a>
+            </Link>
           </button>
 
           <button>
@@ -127,7 +146,7 @@ const Navbar = () => {
           <button
             aria-label="menu"
             className={`${
-              theme === "dark" ? "text-white" : "text-black"
+              theme === "dark" ? "text-white pr-4" : "text-black pr-4"
             } md:hidden`}
             onClick={() => setToggleMenu(true)}
           >
