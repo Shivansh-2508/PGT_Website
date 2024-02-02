f99e77ecf7a79fd9ccfaf5a22e82dbb9071adacf
PrabirKalwani <70889682+PrabirKalwani@users.noreply.github.com>
2024-02-02 17:03:07 +0530

    added new about vision part

--- src/components/OurMission.js
+++ src/components/OurMission.js
@@ -6,43 +6,54 @@ const OurMission = () => {
   return (
     <>
       {" "}
-      <section className='container mx-auto px-5 md:px-16' id='OurMission'>
-        <span className='service-name text-center' style={{ fontSize: "28px" }}>
-          Mission{" "}
-        </span>
-        <h2
-          className='title w-50 text-center py-5'
-          style={{ fontSize: "18px" }}>
-          Impacting the lives of all students who choose to embark on their
-          educational journey with us by prioritize personal attention and care,
-          ensuring that each student receives a tailored learning experience.
-        </h2>
-      </section>
-      <section className='container mx-auto px-5 md:px-16' id='OurMission'>
-        <span className='service-name text-center' style={{ fontSize: "28px" }}>
-          Vision{" "}
-        </span>
-        <h2
-          className='title w-50 text-center py-5'
-          style={{ fontSize: "18px" }}>
-          We envision to revolutionize coaching industry by focusing on
-          imparting education with high value addition and yet be profitable and
-          sustainable.
-        </h2>
-      </section>
-      <section className='container mx-auto px-5 md:px-16' id='OurMission'>
-        <span className='service-name text-center' style={{ fontSize: "28px" }}>
-          About Us{" "}
-        </span>
-        <h2
-          className='title w-50 text-center py-5'
-          style={{ fontSize: "18px" }}>
-          Welcome to Pinnacle, where we blend traditional and modern teaching
-          for academic success and values-driven citizenship. Our passionate
-          educators foster holistic development, guiding students at all levels
-          towards greatness. Join us for a supportive community, achieving
-          excellence beyond the classroom.
-        </h2>
+      <section className='p-6 dark:text-gray-100'>
+        <div className='container grid gap-6 mx-auto text-center lg:grid-cols-2 xl:grid-cols-5'>
+          <div className='w-full px-6 py-1 rounded-md sm:px-12 md:px-16 xl:col-span-2'>
+            <span className='block mb-2 dark:text-rose-800'>
+              Pinnacle Group Tuitions
+            </span>
+            <h1 className='text-3xl font-extrabold dark:text-gray-50'>
+              About Us
+            </h1>
+            <p className='my-4'>
+              <span className='font-medium dark:text-gray-50'>
+                Welcome to Pinnacle,
+              </span>
+              where we blend traditional and modern teaching for academic
+              success and values-driven citizenship. Our passionate educators
+              foster holistic development, guiding students at all levels
+              towards greatness.
+            </p>
+
+            <h1 className='text-3xl font-extrabold dark:text-gray-50'>
+              Our Vision
+            </h1>
+            <p className='my-4'>
+              <span className='font-medium dark:text-gray-50'>
+                We envision to revolutionize coaching industry by focusing on
+                imparting education with high value addition and yet be
+                profitable and sustainable.
+              </span>
+            </p>
+            <h1 className='text-3xl font-extrabold dark:text-gray-50'>
+              Our Mission
+            </h1>
+            <p className='my-4'>
+              <span className='font-medium dark:text-gray-50'>
+                Impacting the lives of all students who choose to embark on
+                their educational journey with us by prioritize personal
+                attention and care, ensuring that each student receives a
+                tailored learning experience.
+              </span>
+            </p>
+          </div>
+
+          <img
+            src='https://source.unsplash.com/random/480x360'
+            alt=''
+            className='object-cover w-full rounded-md xl:col-span-3 dark:bg-gray-500'
+          />
+        </div>
       </section>
     </>
   );
